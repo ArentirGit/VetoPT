49af296d29cd135513cc7efd372696117cfb2152
damjacques <djacques@INFO-LEODAGAN.iut.bx1>
2016-02-03 13:30:09 +0100

    Ajout requête
    Ajout de requête INSERT UPDATE DELETE et une de recherche

--- VetoPTApplication/VetoPTApplication/DataBase/DataBaseManagement.cs
+++ VetoPTApplication/VetoPTApplication/DataBase/DataBaseManagement.cs
@@ -10,20 +10,22 @@ namespace VetoPTApplication.DataBase
     class DataBaseManagement
     {
         string nameDataBase;
+        string ChaineBd;
+        OleDbConnection dbCon;
 
         public DataBaseManagement(string nameDB)
         {
             nameDataBase = nameDB;
+            ChaineBd = "Provider=SQLOLEDB;Data Source=INFO-SIMPLET;Initial Catalog=" + nameDataBase + ";Uid=ETD; Pwd=ETD";
+            dbCon = new OleDbConnection(ChaineBd);
         }
         public List<Animal> SearchAnimals(string name)
         {
             #region Accès sécurisé
             string sql = "Select Code_Animal, Nom_Animal, Poids_Animal FROM Animal "
                        + " WHERE Nom_Animal Like ?  ORDER BY Nom_Animal";
-            nameDataBase = "nom";
-            OleDbConnection dbCon;
-            string ChaineBd = "Provider=SQLOLEDB;Data Source=INFO-SIMPLET;Initial Catalog=" + nameDataBase + ";Uid=ETD; Pwd=ETD";
-            dbCon = new OleDbConnection(ChaineBd);
+            nameDataBase = "nom";       
+            
             dbCon.Open();
             OleDbCommand cmd = new OleDbCommand(sql, dbCon);
             cmd.Parameters.Add("init", OleDbType.VarChar).Value = name;
@@ -43,5 +45,35 @@ namespace VetoPTApplication.DataBase
             return animals;
             #endregion
         }
+
+        public void InsertAnimal(string name,int weight){
+            string insert = "INSERT INTO Animal Values (?,?)";
+            dbCon.Open();
+            OleDbCommand cmd = new OleDbCommand(insert, dbCon);
+            cmd.Parameters.Add("Nom", OleDbType.VarChar).Value = name;
+            cmd.Parameters.Add("Poids", OleDbType.Integer).Value = weight;
+            cmd.ExecuteNonQuery();
+            dbCon.Close();
+        }
+
+        public void UpdateAnimal(int weight,int code){
+            string update = "UPDATE Animal SET Poids_Animal = ? WHERE Code_Animal = ?";
+            dbCon.Open();            
+            OleDbCommand cmd = new OleDbCommand(update, dbCon);
+            cmd.Parameters.Add("Nom", OleDbType.Integer).Value = weight;
+            cmd.Parameters.Add("Code", OleDbType.Integer).Value = code;
+            dbCon.Close();
+        }
+
+        public void DeleteAnimal(int code)
+        {
+            string delete = "DELETE FROM Abonné WHERE Code_Abonné = ?";
+            dbCon.Open();
+            OleDbCommand cmd = new OleDbCommand(delete, dbCon);
+            cmd.Parameters.Add("Code", OleDbType.Integer).Value = code;
+            dbCon.Close();
+        }
+
+
     }
 }
