329064bb0f1e442f3321a3224303057964f56350
Nicolas Sentout <nicodufcbas@hotmail.fr>
2016-02-15 08:25:17 +0100

    ajout requetes BD

--- VetoPTApplication/VetoPTApplication/DataBase/DataBaseManagement.cs
+++ VetoPTApplication/VetoPTApplication/DataBase/DataBaseManagement.cs
@@ -74,6 +74,136 @@ namespace VetoPTApplication.DataBase
             dbCon.Close();
         }
 
+        public void displayAppointment(int code_animal)
+        {
+            string display = "SELECT date, objet FROM Rendez_Vous "
+                            + "JOIN Animal ON Rendez_Vous.id = Animal.id "
+                            + "WHERE Animal.id = ?";
+            dbCon.Open();
+            OleDbCommand cmd = new OleDbCommand(display, dbCon);
+            cmd.Parameters.Add("Code", OleDbType.Integer).Value = code_animal;
+            dbCon.Close(); 
+        }
+
+        public void displayAnimalCare(int code_animal)
+        {
+            string display = "SELECT nom FROM Traitement "
+                            + "JOIN Animal ON Traitement.id = Animal.id "
+                            + "WHERE Animal.id = ?";
+            dbCon.Open();
+            OleDbCommand cmd = new OleDbCommand(display, dbCon);
+            cmd.Parameters.Add("Code", OleDbType.Integer).Value = code_animal;
+            dbCon.Close();
+        }
+
+        public void displayAnimalDetails(int code_animal)
+        {
+            string display = "SELECT * FROM Animal "
+                            + "WHERE Animal.id = ?";
+            dbCon.Open();
+            OleDbCommand cmd = new OleDbCommand(display, dbCon);
+            cmd.Parameters.Add("Code", OleDbType.Integer).Value = code_animal;
+            dbCon.Close();
+        }
+
+        public void insertAnimalv2(string name, int weight, string owner, string birth_date, string specy, string breed)
+        {
+            string insert = "INSERT INTO Animal Values (?,?,?);"
+                          + "INSERT INTO Race Values (?) "
+                           + "JOIN Race on Animal.id = Race.id;"
+                          + "INSERT INTO Personne Values (?) "
+                           + "JOIN Personne on Personne.id = Animal.id;"
+                          + "INSERT INTO Espece Values (?) "
+                           + "JOIN Race on Race.id = Espece.id;";
+                           
+            dbCon.Open();
+            OleDbCommand cmd = new OleDbCommand(insert, dbCon);
+            cmd.Parameters.Add("Nom", OleDbType.VarChar).Value = name;
+            cmd.Parameters.Add("Poids", OleDbType.Integer).Value = weight;
+            cmd.Parameters.Add("Date de naissance", OleDbType.VarChar).Value = birth_date;
+            cmd.Parameters.Add("Race", OleDbType.VarChar).Value = breed;
+            cmd.Parameters.Add("Proprietaire", OleDbType.VarChar).Value = owner;
+            cmd.Parameters.Add("Espece", OleDbType.VarChar).Value = specy;
+            cmd.ExecuteNonQuery();
+            dbCon.Close();
+        }
+
+        public void updateAnimalv2(int weight, int code)
+        {
+            string update = "UPDATE Animal SET Poids_Animal = ? WHERE Code_Animal = ?";
+            dbCon.Open();
+            OleDbCommand cmd = new OleDbCommand(update, dbCon);
+            cmd.Parameters.Add("Nom", OleDbType.Integer).Value = weight;
+            cmd.Parameters.Add("Code", OleDbType.Integer).Value = code;
+            dbCon.Close();
+        }
+
+        public void displayReminders()
+        {
+            string display = "SELECT * FROM Rappel "
+                            + "JOIN Traitement on Traitement.id = Rappel.id "
+                            + "JOIN Animal on Animal.id = Traitement.id "
+                            + "JOIN Personne on Personne.id = Animal.id";
+            dbCon.Open();
+            OleDbCommand cmd = new OleDbCommand(display, dbCon);
+            dbCon.Close();
+        }
+
+        public void insertReminder(string date, string intitule, string code_animal)
+        {
+            string insert = "INSERT INTO Rappel Values (?,?)"
+                            + "JOIN Traitement on Traitement.id = Rappel.id "
+                            + "JOIN Animal on Traitement.id = Animal.id "
+                            + "WHERE Animal.id = ?";
+            dbCon.Open();
+            OleDbCommand cmd = new OleDbCommand(insert, dbCon);
+            cmd.Parameters.Add("Date Rappel", OleDbType.VarChar).Value = date;
+            cmd.Parameters.Add("Intitule", OleDbType.VarChar).Value = intitule;
+            cmd.Parameters.Add("Code Animal", OleDbType.VarChar).Value = code_animal;
+            cmd.ExecuteNonQuery();
+            dbCon.Close();
+        }
+
+        public void updateReminder(int code_reminder, string date, string intitule)
+        {
+            string update = "UPDATE Reminder SET date = ? and Intitule = ? "
+                            + "WHERE id = ?";
+            dbCon.Open();
+            OleDbCommand cmd = new OleDbCommand(update, dbCon);
+            cmd.Parameters.Add("Code Rappel", OleDbType.Integer).Value = code_reminder;
+            cmd.Parameters.Add("Date", OleDbType.VarChar).Value = date;
+            cmd.Parameters.Add("Intitule", OleDbType.VarChar).Value = intitule;
+            dbCon.Close();
+        }
+
+        public void deleteReminder(int code_reminder)
+        {
+            string delete = "DELETE FROM Rappel WHERE id = ?";
+            dbCon.Open();
+            OleDbCommand cmd = new OleDbCommand(delete, dbCon);
+            cmd.Parameters.Add("Code Rappel", OleDbType.Integer).Value = code_reminder;
+            dbCon.Close();
+        }
+
+        public void insertSpecy(string specy_name)
+        {
+            string insert = "INSERT INTO Espece Values (?)";
+            dbCon.Open();
+            OleDbCommand cmd = new OleDbCommand(insert, dbCon);
+            cmd.Parameters.Add("Nom Espece", OleDbType.VarChar).Value = specy_name;
+            cmd.ExecuteNonQuery();
+            dbCon.Close();
+        }
+
+        public void insertBreed(string breed_name)
+        {
+            string insert = "INSERT INTO Race Values (?)";
+            dbCon.Open();
+            OleDbCommand cmd = new OleDbCommand(insert, dbCon);
+            cmd.Parameters.Add("Nom Race", OleDbType.VarChar).Value = breed_name;
+            cmd.ExecuteNonQuery();
+            dbCon.Close();
+        }
 
     }
 }
